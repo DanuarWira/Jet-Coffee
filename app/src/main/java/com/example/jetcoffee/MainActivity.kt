c887acd3a90bf4f2845669b1ec8c14261a7ec6a0
Danuar Wiranata <danuarwr@gmail.com>
2024-07-20 11:06:00 +0700

    Add Banner & Category Item

--- app/src/main/java/com/example/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/example/jetcoffee/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetcoffee.model.dummyCategory
+import com.example.jetcoffee.ui.components.Banner
+import com.example.jetcoffee.ui.components.CategoryItem
+import com.example.jetcoffee.ui.components.SectionText
 import com.example.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetCoffeeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                JetCoffeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun JetCoffeApp() {
+    Column {
+        Banner()
+        SectionText(title = stringResource(id = R.string.section_category))
+        CategoryRow()
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun JetCoffeAppPreview() {
+    JetCoffeApp()
+}
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = Modifier.padding(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CategoryRowPreview() {
     JetCoffeeTheme {
-        Greeting("Android")
+        CategoryRow()
     }
 }
---
+++ app/src/main/java/com/example/jetcoffee/ui/components/Search.kt
@@ -0,0 +1,81 @@
+package com.example.jetcoffee.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetcoffee.R
+import com.example.jetcoffee.ui.theme.JetCoffeeTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search(modifier: Modifier = Modifier) {
+    SearchBar(
+        query = "",
+        onQueryChange = {},
+        onSearch = {},
+        active = false,
+        onActiveChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        },
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder_search))
+        },
+        shape = MaterialTheme.shapes.large,
+        colors = SearchBarDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min = 48.dp)
+    ) {
+
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = Modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.height(160.dp)
+        )
+        Search()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchPreview() {
+    JetCoffeeTheme {
+        Search()
+    }
+}
